a546f519d87abffc15f6c07af38a3568a1b329d5
AmanNBATech <amannba0427@gmail.com>
2023-07-04 21:30:30 +0530

    new gane

--- game.py
+++ game.py
@@ -1,11 +1,15 @@
 import random
+level=int(input("enter any level you want to play"))
+print(level)
+if (level)==1:
 
-a=random.randint(1,7)
-for i in range(1,4):
+ for i in range(1,4):
     
     
     
- if i==1:
+  if i==1:
+      
+      a=random.randint(1,4)
       b=int(input("play first chance "))
       print("this is your first chance") 
       if (a==b):
@@ -17,7 +21,7 @@ for i in range(1,4):
          print("you lose")
          print(a,b)
          
- if i==2:
+  if i==2:
         a=random.randint(1,19)
         b=int(input("play second chance "))
        
@@ -28,7 +32,7 @@ for i in range(1,4):
         else:
           print("you loose second time") 
           print(a,b)   
- if i==3:
+  if i==3:
         a=random.randint(1,19)
         b=int(input("play Third chance "))
         
@@ -40,10 +44,86 @@ for i in range(1,4):
          print("you loose third time")  
          print(a,b)
 
-      
+if level==2:
+ a=random.randint(1,50)
+ for i in range(1,4):
     
+    
+    
+  if i==1:
+      b=int(input("play first chance "))
+      print("this is your first chance") 
+      if (a==b):
+         print("you won")
 
+         print(a,b)
+         break
+      else:
+         print("you lose")
+         print(a,b)
+         
+  if i==2:
+        a=random.randint(1,50)
+        b=int(input("play second chance "))
+       
+        if (a==b):
+          print("you won")
+          print(a,b)
+          break
+        else:
+          print("you loose second time") 
+          print(a,b)   
+  if i==3:
+        a=random.randint(1,50)
+        b=int(input("play Third chance "))
+        
+        if (a==b):
+         print("you won")
+         print(a,b)
+         break
+        else:
+         print("you loose third time")  
+         print(a,b)
+if level==3:
+     a=random.randint(1,100)
+for i in range(1,4):
+    
+    
+    
+  if i==1:
+      b=int(input("play first chance "))
+      print("this is your first chance") 
+      if (a==b):
+            print("you won")
 
+            print(a,b)
+            break
+      else:
+         print("you lose")
+         print(a,b)
+         
+  if i==2:
+        a=random.randint(1,100)
+        b=int(input("play second chance "))
+       
+        if (a==b):
+          print("you won")
+          print(a,b)
+          break
+        else:
+          print("you loose second time") 
+          print(a,b)   
+  if i==3:
+        a=random.randint(1,100)
+        b=int(input("play Third chance "))
+        
+        if (a==b):
+         print("you won")
+         print(a,b)
+         break
+        else:
+         print("you loose third time")  
+         print(a,b)
 
 
 
